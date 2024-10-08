68212c8e9d37487d01217ab0a7bc02805d23575f
Daniela <danielaquinones.1203@gmail.com>
2024-10-08 14:21:13 -0500

    Primer commit del proyecto

---
+++ Program.cs
@@ -0,0 +1,7 @@
+using MyLibrary;
+Employee Daniela = new Secretary();
+Daniela.Name = "Daniela";
+Employee Pedro = new Manager ();
+Pedro.Name = "Pedro";
+Developer Miguel = new Developer () ;
+Console WriteLine($"Dias: {Miguel.GetPaymentDays()}*);
